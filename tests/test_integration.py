a0e05b2b9da50b5443751e66d7c8786074da3751
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-05-15 11:34:34 +0100

    Fix to make multiplicity increase dependent on all atoms being close rather than any

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -19,58 +19,58 @@ def test_derive_alignment_heirarchy(
     ...
 
 
-@pytest.mark.order(after="test_collator_upload_1")
-def test_aligner_upload_1(constants, assemblies_file, upload_1_dir):
-    a = Aligner(upload_1_dir, constants.METADATA_FILE, assemblies_file)
-    num_errors, num_warnings = a.validate()
-
-    if num_errors:
-        print("There are errors, cannot continue")
-        print(a.logger.errors)
-        exit(1)
-    else:
-        a.run()
-
-
-@pytest.mark.order(after="test_aligner_upload_1")
-def test_collator_upload_2(
-    constants,
-    test_dir,
-    upload_2_dir,
-    upload_2_data_dir,
-    config_2_file,
-):
-    c = Collator(
-        str(config_2_file),
-    )
-    logger = c.logger
-
-    meta = c.validate()
-
-    if meta is None or len(logger.errors) > 0:
-        print("There are errors, cannot continue")
-        print(logger.errors)
-        exit(1)
-    else:
-        c.run(meta)
-    assert (
-        len(meta[Constants.META_XTALS]["Mpro-i0130"][Constants.META_XTAL_FILES].get(Constants.META_BINDING_EVENT, {}))
-        != 0
-    )
-
-
-@pytest.mark.order(after="test_collator_upload_2")
-def test_aligner_upload_2(constants, assemblies_file, upload_2_dir):
-    a = Aligner(upload_2_dir, constants.METADATA_FILE, assemblies_file)
-    num_errors, num_warnings = a.validate()
-
-    if num_errors:
-        print("There are errors, cannot continue")
-        print(a.logger.errors)
-        exit(1)
-    else:
-        a.run()
-    assert "Mpro-i0130" in [x.name for x in (Path(upload_2_dir) / "aligned_files").glob("*")]
+# @pytest.mark.order(after="test_collator_upload_1")
+# def test_aligner_upload_1(constants, assemblies_file, upload_1_dir):
+#     a = Aligner(upload_1_dir, constants.METADATA_FILE, assemblies_file)
+#     num_errors, num_warnings = a.validate()
+#
+#     if num_errors:
+#         print("There are errors, cannot continue")
+#         print(a.logger.errors)
+#         exit(1)
+#     else:
+#         a.run()
+#
+#
+# @pytest.mark.order(after="test_aligner_upload_1")
+# def test_collator_upload_2(
+#     constants,
+#     test_dir,
+#     upload_2_dir,
+#     upload_2_data_dir,
+#     config_2_file,
+# ):
+#     c = Collator(
+#         str(config_2_file),
+#     )
+#     logger = c.logger
+#
+#     meta = c.validate()
+#
+#     if meta is None or len(logger.errors) > 0:
+#         print("There are errors, cannot continue")
+#         print(logger.errors)
+#         exit(1)
+#     else:
+#         c.run(meta)
+#     assert (
+#         len(meta[Constants.META_XTALS]["Mpro-i0130"][Constants.META_XTAL_FILES].get(Constants.META_BINDING_EVENT, {}))
+#         != 0
+#     )
+#
+#
+# @pytest.mark.order(after="test_collator_upload_2")
+# def test_aligner_upload_2(constants, assemblies_file, upload_2_dir):
+#     a = Aligner(upload_2_dir, constants.METADATA_FILE, assemblies_file)
+#     num_errors, num_warnings = a.validate()
+#
+#     if num_errors:
+#         print("There are errors, cannot continue")
+#         print(a.logger.errors)
+#         exit(1)
+#     else:
+#         a.run()
+#     assert "Mpro-i0130" in [x.name for x in (Path(upload_2_dir) / "aligned_files").glob("*")]
 
 
 # @pytest.mark.order(after="test_aligner_upload_2")
