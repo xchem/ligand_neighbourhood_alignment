c005ce08f09f8c1a1e9a3333eabacb1c0e897572
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-05-15 16:43:46 +0100

    Fix to make multiplicity increase dependent on all atoms being close rather than any

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -29,7 +29,7 @@ def test_chain_to_biochain(
             for generator in assembly.generators:
                 biochain = alignment_heirarchy._chain_to_biochain(generator.chain, xtalform, assemblies)
                 rprint(
-                    f'Chain to Biochain: {xtalform_name} : {assembly_name} : {generator.chain} : dimer -> {biochain}'
+                    f'Chain to Biochain: {xtalform_name} : {assembly_name} : {generator.chain} -> {biochain}'
                 )
 
 # @pytest.mark.order(after="test_collator_upload_1")
