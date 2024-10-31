6d6d5d88ca38d4e1684fd9168859b24557182ddd
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-10-31 17:08:41 +0000

    Fix for A71

--- src/ligand_neighbourhood_alignment/cli.py
+++ src/ligand_neighbourhood_alignment/cli.py
@@ -1039,7 +1039,7 @@ def _update_xtalform_sites(
                 xtalform_site_id = "/".join(centroid_residue)
                 xtalform_site = dt.XtalFormSite(
                     xtalform_name,
-                    crystalform_observation_centroids[xtalform_name][centroid_residue][1],
+                    crystalform_observation_centroids[xtalform_name][centroid_residue][0],
                     canonical_site_id,
                     asigned_observation_ids,
                 )
